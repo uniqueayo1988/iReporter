a1b4b41e35da602af7ad9cc743eae57648214c2f
Adebayo Ayo-Oluwa <ayooluwabayo@gmail.com>
2018-12-03 12:36:17 +0100

    [#162374354 Bug] Refactor create record

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,7 +1,6 @@
-let sideNav = document.getElementById("mySidenav");
+let sideNav = document.getElementById('mySidenav');
 let btn = document.getElementById('btn');
-
-let openNav = () => sideNav.style.width = "250px";
-let closeNav = () => sideNav.style.width = "0";
-
-btn.onclick = () => location.href = 'profile.html';
+let openNav = () => sideNav.style.width = '250px';
+let closeNav = () => sideNav.style.width = '0';
+let geoData = document.getElementById('geo-data');
+let geoBtn = document.getElementById('geo-btn');
