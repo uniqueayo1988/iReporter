ac980a3bcabf7c6ff3e5a4ff1c69f6db4ddbeb72
Adebayo Ayo-Oluwa <ayooluwabayo@gmail.com>
2018-12-05 12:26:12 +0100

    [#162442894 Chore] Add middleware to API routes

--- server/controllers/Red-flag.js
+++ server/controllers/Red-flag.js
@@ -2,16 +2,13 @@ import RedflagModel from '../models/Red-flag';
 
 const Redflag = {
   create(req, res) {
-    if (!req.body.location || !req.body.comment) {
-      return res.status(400).send({ message: 'Location and comment fields are required' });
-    }
-
     const redflag = RedflagModel.create(req.body);
     return res.status(201).send({
       status: 201,
       data: [{
         id: redflag.id,
-        message: 'Created red-flag record'
+        message: 'Created red-flag record',
+        redflag
       }]
     });
   },
@@ -26,10 +23,6 @@ const Redflag = {
 
   getOne(req, res) {
     const redflag = RedflagModel.findOne(req.params.id);
-    if (!redflag) {
-      return res.status(404).send({ message: 'Red-flag record not found' });
-    }
-
     return res.status(200).send({
       status: 200,
       data: [redflag]
@@ -38,22 +31,14 @@ const Redflag = {
 
   update(req, res) {
     const redflag = RedflagModel.findOne(req.params.id);
-
-    if (!redflag) {
-      return res.status(404).send({ message: 'Red-flag record not found' });
-    }
-
-    if (!req.body.location && !req.body.comment) {
-      return res.status(400).send({ message: 'Field can not be blank' });
-    }
-
     RedflagModel.update(req.params.id, req.body);
     if (req.body.location) {
       return res.status(200).send({
         status: 200,
         data: [{
           id: redflag.id,
-          message: 'Updated red-flag record\'s location'
+          message: 'Updated red-flag record\'s location',
+          redflag
         }]
       });
     }
@@ -62,18 +47,14 @@ const Redflag = {
       status: 200,
       data: [{
         id: redflag.id,
-        message: 'Updated red-flag record\'s comment'
+        message: 'Updated red-flag record\'s comment',
+        redflag
       }]
     });
   },
 
   delete(req, res) {
     const redflag = RedflagModel.findOne(req.params.id);
-
-    if (!redflag) {
-      return res.status(404).send({ message: 'redflag not found' });
-    }
-
     RedflagModel.delete(req.params.id);
     return res.status(200).send({
       status: 204,
--- server/routers/router.js
+++ server/routers/router.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import Redflag from '../controllers/Red-flag';
+import Validator from '../middleware/validator';
 
 const router = express.Router();
 
-router.post('/red-flags', Redflag.create);
+router.post('/red-flags', Validator.create, Redflag.create);
 router.get('/red-flags', Redflag.getAll);
-router.get('/red-flags/:id', Redflag.getOne);
-router.patch('/red-flags/:id/location', Redflag.update);
-router.patch('/red-flags/:id/comment', Redflag.update);
-router.delete('/red-flags/:id', Redflag.delete);
+router.get('/red-flags/:id', Validator.getOne, Redflag.getOne);
+router.patch('/red-flags/:id/location', Validator.update, Redflag.update);
+router.patch('/red-flags/:id/comment', Validator.update, Redflag.update);
+router.delete('/red-flags/:id', Validator.delete, Redflag.delete);
 
 export default router;
