d6b65602d91bdbd3db412289c69f0a42f4aa33b5
Adebayo Ayo-Oluwa <ayooluwabayo@gmail.com>
2018-12-11 16:04:56 +0100

    [#162579097 Chore] Implement authentication to be used for all Intervention and Red-flag routes

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 import express from 'express';
-import redflagRoutes from './routers/router';
+import dotenv from 'dotenv';
+import 'babel-polyfill';
+import userRoutes from './routers/userRouter';
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/api/v1', redflagRoutes);
+app.use('/api/v1', userRoutes);
 
 app.listen(port, () => {
-  console.log(`Server Started at port ${port}`);
+  console.log(`Server now started at port ${port}`);
 });
-
-// app.listen(port);
 
 export default app;
