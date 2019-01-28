0bbfa65d14aebab16d5f92ca108e3a3fbabca73b
Adebayo Ayo-Oluwa <ayooluwabayo@gmail.com>
2019-01-28 11:51:30 +0100

    [Bug #163514221] Change helper case

---
+++ server/middleware/helper.js
@@ -0,0 +1,43 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const Helper = {
+  /**
+   * Hash Password Method
+   * @param {string} password
+   * @returns {string} return hashed password
+   */
+  hashPassword(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  },
+
+  /**
+   * comparePassword
+   * @param {string} hashPassword
+   * @param {string} password
+   * @returns {Boolean} return True or False
+   */
+  comparePassword(hashPassword, password) {
+    return bcrypt.compareSync(password, hashPassword);
+  },
+
+  /**
+   * Gnerate Token
+   * @param {string} id
+   * @returns {string} token
+   */
+
+  generateToken(id) {
+    const token = jwt.sign({
+      userId: id
+    }, process.env.SECRET,
+    {
+      expiresIn: '1d'
+    });
+    return token;
+  }
+};
+
+export default Helper;
+
+// credit: https://www.codementor.io/olawalealadeusi896/building-a-simple-api-with-nodejs-expressjs-postgresql-db-and-jwt-3-mke10c5c5
